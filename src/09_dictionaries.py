f458c7de9653a3ded6e0c5748a99c4d4156d3f7d
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-05-28 16:53:00 -0400

    Completes dictionary challenges

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,10 +35,26 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+home = {"lat": -90, "lon":12, "name":"home"}
+waypoints.append (home)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
+waypoints[0]["lon"] = -130
+waypoints[0]["name"] = "not a real place"
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for i in waypoints:
+    lat = i["lat"]
+    lon = i["lon"]
+    name = i["name"]
+    n_or_s = ""
+    e_or_w = ""
+    if lat is not 0:
+        n_or_s = "N" if lat > 0 else "S"
+    if lon is not 0:
+        e_or_w = "E" if lon > 0 else "W"
+
+    print(f"{name}: {abs(lat)}{n_or_s}, {abs(lon)}{e_or_w}")
