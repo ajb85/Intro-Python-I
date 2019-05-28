b9d0bafc5d13f8ca38342dd7b7a2e8af56f56906
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-05-28 13:19:57 -0400

    Fixes overflow issues

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,9 +2,7 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
-from math import pow
 
-# OverflowError: math range error
-print(pow(2,65536)) 
+print(2 ** 65536 )
 
 # JS -> Infinity
